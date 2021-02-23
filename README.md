c8b4fcb1a51f77f2dbd4863bdbe8e1a72b68e51e
iurisilvestre <iurisdesign@gmail.com>
2021-02-23 14:46:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-script-calculator
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/java-script-calculator)
---
+++ index.js
@@ -0,0 +1,39 @@
+// Import stylesheets
+import "./style.css";
+
+//Globals
+let inputString = "";
+
+// DOM
+const prevResult = document.querySelector("#prev-operation");
+const currentResult = document.querySelector("#operation");
+const allButtons = document.querySelectorAll("button");
+const resultBtn = document.querySelector("#equal");
+const resetBtn = document.querySelector("#reset");
+
+function result() {
+  if (inputString === "") {
+    currentResult.innerHTML = "0";
+  } else {
+    inputString = eval(inputString);
+    currentResult.innerHTML = inputString;
+  }
+}
+
+function allClear() {
+  inputString = "";
+  currentResult.innerHTML = "0";
+}
+
+allButtons.forEach(item => {
+  item.addEventListener("click", function(e) {
+    inputString += e.target.value;
+    currentResult.innerHTML = inputString
+      .replace(/\*/g, "")
+      .replace(/\//g, "&divide");
+  });
+});
+
+//Events
+resultBtn.addEventListener("click", result);
+resetBtn.addEventListener("click", allClear);
